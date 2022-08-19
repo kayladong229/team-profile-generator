8dcb1388c70e8d9bb4a3d49ff4cf683dd3327b9f
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-08-19 12:51:40 -0700

    Added initial files and code for all classes

---
+++ index.js
@@ -0,0 +1,7 @@
+// Declare constants
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const engineer = require('./lib/engineer')
+const intern = require('./lib/intern')
+const manager = require('./lib/manager')
